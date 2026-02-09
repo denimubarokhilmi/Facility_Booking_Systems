cc0b40513c6ef18a2a08f6534b232bde0e5fec8f
denimubarokhilmi <hilmimubarokdeni@gmail.com>
2026-02-09 16:36:01 +0700

    feat: configure Vue Router for multi-role navigation

---
+++ src/router/router.js
@@ -0,0 +1,68 @@
+import { createRouter, createWebHistory } from "vue-router";
+import welcome from "@/layouts/welcome.vue";
+import login from "@/layouts/login.vue";
+import admin_page from "@/page/admin_page.vue";
+import user_page from "@/page/user_page.vue";
+import officer_page from "@/page/officer_page.vue";
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "welcome",
+      component: welcome,
+      meta: {
+        title: "Welcome",
+        hideLayout: true,
+      },
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: login,
+      meta: { title: "Login", hideLayout: true },
+    },
+    {
+      path: "/admin_page",
+      name: "admin",
+      component: admin_page,
+      meta: { title: "Admin", requiresAuth: true },
+    },
+    {
+      path: "/mahasiswa_page",
+      name: "mhasiswa",
+      component: user_page,
+      meta: { title: "Mahasiswa", requiresAuth: true },
+    },
+    {
+      path: "/officer_page",
+      name: "officer",
+      component: officer_page,
+      meta: { title: "Officer", requiresAuth: true },
+    },
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+
+    return { top: 0 };
+  },
+});
+
+router.beforeEach((to, from, next) => {
+  const token = document.cookie;
+  // currentPage.value = to.name;
+  document.title = to.meta.title;
+
+  if (to.meta.requiresAuth && token.length == 0) {
+    return next("/login");
+  }
+
+  // if (token != 0) {
+  //   return next("/");
+  // }
+
+  next();
+});
+export default router;
